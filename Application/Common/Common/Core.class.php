8d6014c5f6910e34aefc63fd7bc0d59e25ec9433
Fennay <feng@fennay.com>
2015-07-22 17:39:14 +0800

    删除，随机颜色

--- Application/Common/Common/Core.class.php
+++ Application/Common/Common/Core.class.php
@@ -11,14 +11,25 @@
 function p($s){
     dump($s,true,'',false);
 }
-
 /**
- * [fny_ueditor 导入编辑器]
- * @date   2015-07-17
+ * [fny_array_rand 返回数组中的随机值]
+ * @date   2015-07-22
  * @author Fennay [<feng@fennay.com>]
- * @return [type]     [输出编辑器]
+ * @param  [type]     $arr [一维数组]
+ * @param  string     $num [返回一个，或者多个值]
+ * @return [type]          [description]
  */
-function fny_ueditor(){
-    // 编辑器引入相应的样式
-    // 
+function fny_array_rand($arr,$num='1'){
+    if($num == 1){
+        return $arr[array_rand($arr)];
+    }else{
+        // 返回多个值
+        $rs = array_rand($arr,$num);
+        $tmp = array();
+        // 遍历结果转换成对应值
+        foreach ($rs as $v) {
+            $tmp[] = $arr[$v];
+        }
+        return $tmp;
+    }
 }
--- Application/Padmin/Controller/ArticleController.class.php
+++ Application/Padmin/Controller/ArticleController.class.php
@@ -22,8 +22,13 @@ class ArticleController extends AdminController {
      * @return [type]     [description]
      */
     public function index(){
-        $article = D('Article')->relation(true)->select();
+        $map['status'] = array('lt',2);
+        $count = D('Article')->where($map)->count();
+        $page = new \Think\Page($count,10);
+        $article = D('Article')->where($map)->relation(true)->limit($page->firstRow.','.$page->listRows)->select();
         // p($article);
+        $this->list = $article;
+        $this->page = $page->show();
         $this->display();
     }
     /**
@@ -41,7 +46,21 @@ class ArticleController extends AdminController {
      * @return [type]     [description]
      */
     public function edit(){
-        $this->display();
+        $aid = I('get.aid');
+        $map['id'] = $aid;  //文章ID
+        // $article = D('Article')->where($map)->relation(true)->find();
+        $article = M('Article')->where($map)->find();
+        $cate = M('Article_cate')->where($map)->getField('cid',true);
+        $tag = M('Article_tag')->where($map)->getField('tid',true);
+        $content = M('Article_content')->where($map)->getField('content');
+
+        //将分类，标签合并到Article中
+        $article['cates'] = implode($cate, ',');
+        $article['tags'] = implode($tag, ',');
+        $article['content'] = $content;
+        //分配到前端页面
+        $this->vo = $article;
+        $this->display('add');
     }
 
     /**
@@ -115,11 +134,33 @@ class ArticleController extends AdminController {
             p($tagData);
         }
 
-
         //返回值
         $result['status'] = 1;
         $result['msg'] = '数据修改成功';
         $result['url'] = '';
         echo json_encode($result,true);
+    }
+
+    /**
+     * [delete 删除文章]
+     * @date   2015-07-22
+     * @author Fennay [<feng@fennay.com>]
+     * @return [type]     [description]
+     */
+    public function delete(){
+        $aid = I('post.aid');
+        //删除文章，设置状态为2
+        $rs = M('Article')->where(array('id'=>$aid))->setField('status',2);
+        if($rs == false){
+            $result['status'] = -1;
+            $result['msg'] = '数据删除失败';
+            $result['url'] = '';
+        }else{
+            $result['status'] = 1;
+            $result['msg'] = '数据删除成功';
+            $result['url'] = '';
+        }
+        echo json_encode($result,true);
+        exit();
     }
 }
